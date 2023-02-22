3f5c5664b7cc0c2c2589ab6ea485d4fc4e0ff0bf
Enas <enasasmar882@gmail.com>
2023-02-22 11:47:02 +0200

    fixes service provider

--- src/StripeServiceProvider.php
+++ src/StripeServiceProvider.php
@@ -2,22 +2,28 @@
 
 namespace Corals\Modules\Payment\Stripe;
 
+use Corals\Foundation\Providers\BasePackageServiceProvider;
 use Corals\Modules\Payment\Stripe\Providers\StripeRouteServiceProvider;
-use Illuminate\Support\ServiceProvider;
 use Corals\Settings\Facades\Modules;
 
-class StripeServiceProvider extends ServiceProvider
+class StripeServiceProvider extends BasePackageServiceProvider
 {
+    /**
+     * @var
+     */
     protected $defer = false;
+    /**
+     * @var
+     */
+    protected $packageCode = 'corals-payment-stripe';
 
     /**
      * Bootstrap the application events.
      *
      * @return void
      */
-    public function boot()
+    public function bootPackage()
     {
-        $this->registerModulesPackages();
     }
 
     /**
@@ -25,7 +31,7 @@ class StripeServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function registerPackage()
     {
         $this->app->register(StripeRouteServiceProvider::class);
     }
